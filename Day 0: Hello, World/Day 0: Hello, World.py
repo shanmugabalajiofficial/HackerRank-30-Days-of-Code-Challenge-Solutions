3963eb13d464736d7f2907957b8413ba1be6a807
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-16 09:42:56 +0530

    Create Day 0: Hello, World.py

---
+++ Day 0: Hello, World/Day 0: Hello, World.py
@@ -0,0 +1,10 @@
+#Day 0: Hello, World
+
+
+inputString = input() # get a line of input from stdin and save it to our variable
+
+# Your first line of output goes here
+print('Hello, World.')
+
+# Write the second line of output
+print(inputString)
