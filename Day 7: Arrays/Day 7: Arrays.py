c82fb75f0479f0c36da872ed0a2733af5042eef2
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-22 18:06:00 +0530

    Create Day 7: Arrays.py

---
+++ Day 7: Arrays/Day 7: Arrays.py
@@ -0,0 +1,18 @@
+#Day 7: Arrays
+
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+print(*arr[::-1])
