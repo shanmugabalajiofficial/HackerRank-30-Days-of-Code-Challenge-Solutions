c97feb040f9952a606a9dcb92c29e747c16ea756
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-05-23 09:32:42 +0530

    Update Day 8: Dictionaries and Maps.py

--- Day 8: Dictionaries and Maps/Day 8: Dictionaries and Maps.py
+++ Day 8: Dictionaries and Maps/Day 8: Dictionaries and Maps.py
@@ -2,14 +2,27 @@
 
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-n = int(input())
-phoneBook = {}
-for i in range(0, n):
-    key, value = input().split()
-    phoneBook[key]=value
-for i in range(0, n):
-    name = input()
-    if name in phoneBook:
-        print(name +'=' + phoneBook[name])
-    else:
-        print('Not found')
+try:
+    n = int(input())
+    phoneBook = {}
+    
+    # Input phone book entries
+    for i in range(n):
+        key, value = input().split()
+        phoneBook[key] = value
+    
+    # Query phone book
+    while True:
+        try:
+            name = input()
+            if not name:  # Check for empty input, exit loop if encountered
+                break
+            if name in phoneBook:
+                print(name + '=' + phoneBook[name])
+            else:
+                print('Not found')
+        except EOFError:  # Handle end of input
+            break
+
+except Exception as e:
+    print("Error:", e)
