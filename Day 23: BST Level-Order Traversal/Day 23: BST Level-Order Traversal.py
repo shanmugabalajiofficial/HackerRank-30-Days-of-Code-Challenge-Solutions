519dae2c789c6f24d7b4ec3f4ff6bb1d53f6474a
shanmugabalajiofficial <shanmugabalajiofficial@gmail.com>
2024-06-07 09:50:26 +0530

    Create Day 23: BST Level-Order Traversal.py

---
+++ Day 23: BST Level-Order Traversal/Day 23: BST Level-Order Traversal.py
@@ -0,0 +1,38 @@
+#Day 23: BST Level-Order Traversal
+
+
+import sys
+
+class Node:
+    def __init__(self,data):
+        self.right=self.left=None
+        self.data = data
+class Solution:
+    def insert(self,root,data):
+        if root==None:
+            return Node(data)
+        else:
+            if data<=root.data:
+                cur=self.insert(root.left,data)
+                root.left=cur
+            else:
+                cur=self.insert(root.right,data)
+                root.right=cur
+        return root
+
+
+    def levelOrder(self, root):
+        stack = [root]
+        while stack:
+            node = stack.pop(0)
+            if node:
+                print(node.data, end=" ")
+                stack.append(node.left)
+                stack.append(node.right)
+T=int(input())
+myTree=Solution()
+root=None
+for i in range(T):
+    data=int(input())
+    root=myTree.insert(root,data)
+myTree.levelOrder(root)
